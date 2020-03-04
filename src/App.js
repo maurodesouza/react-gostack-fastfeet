c12e58ae651d6b82a7a520919eb4a01e8a8f8313
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-04 18:24:18 -0300

    ✨  feat: config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import Routes from '~/routes';
+import history from '~/services/history';
 
 export default function App() {
-  return <h1>Hmm ... Hellow ? </h1>;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
---
+++ src/routes/Route.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) return <Redirect to="/" />;
+
+  if (signed && !isPrivate) return <Redirect to="/dashboard" />;
+
+  return <Route component={Component} {...rest} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
