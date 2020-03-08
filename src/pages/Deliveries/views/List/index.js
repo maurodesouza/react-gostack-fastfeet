070f1a842e207dcd93dd740c5d89e68dd0640ee8
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-07 22:26:00 -0300

    ✨  feat: add list view to deliveries

--- src/pages/Deliveries/views/List/index.js
+++ src/pages/Deliveries/views/List/index.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import HeaderView from '~/components/HeaderView';
+import HeaderViewInput from '~/components/HeaderView/HeaderViewInput';
+import HeaderViewRegisterButton from '~/components/HeaderView/HeaderViewRegisterButton';
+import TableList from '~/components/TableList';
+
+import api from '~/services/api';
+import { Container } from './styles';
 
 export default function List() {
-  return <h1>Hmmm ... Hellow ?</h1>;
+  const [deliveries, setDeliveries] = useState([]);
+
+  useEffect(() => {
+    const loadDeliveries = async () => {
+      const response = await api.get('/deliveries', {
+        params: {
+          q: '',
+        },
+      });
+      const data = response.data.map(delivery => ({
+        ...delivery,
+        idFormatted: `#${`00${delivery.id}`.slice(-2)}`,
+      }));
+
+      setDeliveries(data);
+    };
+
+    loadDeliveries();
+  }, []);
+
+  return (
+    <Container teste={deliveries}>
+      <HeaderView title="Gerenciando encomendas">
+        <HeaderViewInput placeholder="Buscar por encomendas" />
+        <HeaderViewRegisterButton />
+      </HeaderView>
+
+      <TableList
+        thead={[
+          'ID',
+          'Destinatário',
+          'Entregador',
+          'Cidade',
+          'Estado',
+          'Status',
+          'Ações',
+        ]}
+      >
+        {deliveries.map(delivery => (
+          <tr key={delivery.id}>
+            <td>{delivery.idFormatted}</td>
+            <td>{delivery.recipient.name}</td>
+            <td>{delivery.deliveryman.name}</td>
+            <td>{delivery.recipient.city}</td>
+            <td>{delivery.recipient.state}</td>
+            <td>Status</td>
+            <td>Ações</td>
+          </tr>
+        ))}
+      </TableList>
+    </Container>
+  );
 }
---
+++ src/pages/Deliveries/views/List/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 30px auto 0;
+  padding-bottom: 30px;
+  max-width: 1200px;
+`;
