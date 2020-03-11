77e271016ec683101207fbeab531be873b1866de
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-11 17:17:12 -0300

    ⚡  improve: clear search field on select submit

--- src/components/HeaderView/HeaderViewForm/index.js
+++ src/components/HeaderView/HeaderViewForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { MdSearch } from 'react-icons/md';
 
@@ -9,13 +9,17 @@ import { Form, SearchWrapper } from './styles';
 import Input from '~/components/Form/Input';
 
 export default function HeaderForm({ onSearch, ...rest }) {
+  const formRef = useRef(null);
+
   const handleSubmit = ({ search = '', status }) => {
     onSearch(search, status);
+
+    if (search === '') formRef.current.setFieldValue('search', '');
   };
 
   return (
     <>
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <SearchWrapper>
           <button type="submit" onSubmit={handleSubmit}>
             <MdSearch size={20} color="#999" />
