37d92f1b07d68c76fca9068798a75f8f941fc68f
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-14 00:43:41 -0300

    ✨  feat: create view/form layout of deliveries

--- src/pages/Deliveries/views/Form/index.js
+++ src/pages/Deliveries/views/Form/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import HeaderView from '~/components/HeaderView';
+import HeaderViewBackButton from '~/components/HeaderView/HeaderViewBackButton';
+import HeaderViewSaveButton from '~/components/HeaderView/HeaderViewSaveButton';
+import Select from '~/components/Form/Select';
+
+import { Container, UnForm, SelectWrapper, Input } from './styles';
 
 export default function Form() {
-  return <div />;
+  return (
+    <Container>
+      <HeaderView title="Cadastro de encomendas" isToForm>
+        <HeaderViewBackButton />
+        <HeaderViewSaveButton />
+      </HeaderView>
+      <UnForm>
+        <SelectWrapper>
+          <Select
+            placeholder="Nome do destinatário ..."
+            name="recipient_id"
+            label="Destinatário"
+          />
+          <Select
+            placeholder="Nome do entregador ..."
+            name="deliveryman_id"
+            label="Entregador"
+          />
+        </SelectWrapper>
+        <Input
+          placeholder="Nome do Produto ..."
+          label="Nome do produto"
+          name="product"
+        />
+      </UnForm>
+    </Container>
+  );
 }
---
+++ src/pages/Deliveries/views/Form/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { Form } from '@unform/web';
+import input from '~/components/Form/Input';
+
+export const Container = styled.div`
+  margin: 30px auto;
+  width: 900px;
+`;
+
+export const UnForm = styled(Form)`
+  width: 100%;
+  background: #fff;
+  border-radius: 5px;
+  padding: 30px;
+  margin-top: 30px;
+`;
+
+export const Input = styled(input)`
+  background: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  align-items: center;
+  padding: 10px 15px;
+  color: #999;
+  width: 100%;
+  height: 45px;
+
+  &::placeholder {
+    color: #999;
+  }
+`;
+
+export const SelectWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+  margin-bottom: 15px;
+`;
