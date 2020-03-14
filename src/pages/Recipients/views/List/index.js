4b9a93d2fcfdc84ef59cf74f84bfb5192ebb4a95
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-14 02:41:18 -0300

    ✨  feat: create the view/list of recipients

---
+++ src/pages/Recipients/views/List/index.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+
+import MenuActions from '~/components/MenuActions';
+import Pagination from '~/components/Pagination';
+import HeaderView from '~/components/HeaderView';
+import HeaderViewForm from '~/components/HeaderView/HeaderViewForm';
+import HeaderViewRegisterButton from '~/components/HeaderView/HeaderViewRegisterButton';
+import TableList from '~/components/TableList';
+
+import ufConversor from '~/util/ufConversor';
+import api from '~/services/api';
+
+import { Container } from './styles';
+
+export default function List({ match }) {
+  const [recipients, setRecipients] = useState([]);
+
+  const [q, setQ] = useState('');
+  const [page, setPage] = useState(1);
+
+  const [totalPages, setTotalPages] = useState(0);
+
+  const onSearch = search => {
+    setQ(search);
+    setPage(1);
+  };
+
+  const backPage = () => setPage(page - 1);
+
+  const nextPage = () => setPage(page + 1);
+
+  const loadRecipients = useCallback(async () => {
+    window.scroll({
+      behavior: 'smooth',
+      top: 0,
+    });
+
+    const response = await api.get('/recipient', {
+      params: {
+        q,
+        page,
+      },
+    });
+
+    const data = response.data.map(recipient => ({
+      ...recipient,
+      idFormatted: `#${`00${recipient.id}`.slice(-2)}`,
+      address: `${recipient.street}, ${recipient.number}, ${
+        recipient.city
+      } - ${ufConversor(recipient.state)}`,
+    }));
+
+    const pageTotal = Math.ceil(response.headers['x-total-count'] / 10);
+
+    setRecipients(data);
+    setTotalPages(pageTotal);
+  }, [page, q]);
+
+  useEffect(() => {
+    loadRecipients();
+  }, [loadRecipients]);
+
+  return (
+    <Container>
+      <HeaderView title="Gerenciando destinatários">
+        <HeaderViewForm
+          searchOnly
+          placeholder="Buscar por destinatários"
+          onSearch={onSearch}
+        />
+        <HeaderViewRegisterButton path={match.path} />
+      </HeaderView>
+
+      <TableList thead={['ID', 'Nome', 'Endereço', 'Ações']}>
+        {recipients.map(recipient => (
+          <tr key={recipient.id}>
+            <td>{recipient.idFormatted}</td>
+
+            <td>{recipient.name}</td>
+
+            <td>{recipient.address}</td>
+
+            <td>
+              <MenuActions
+                noView
+                path={match.path}
+                id={recipient.id}
+                load={loadRecipients}
+              />
+            </td>
+          </tr>
+        ))}
+      </TableList>
+
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        backPage={backPage}
+        nextPage={nextPage}
+      />
+    </Container>
+  );
+}
+
+List.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string,
+  }).isRequired,
+};
---
+++ src/pages/Recipients/views/List/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 30px auto 0;
+  padding-bottom: 50px;
+  max-width: 1200px;
+`;
