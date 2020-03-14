81f66727ba611e57e8bb9d89b5778070481273c6
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-14 00:01:30 -0300

    ⚡  improve: update to receive the label props

--- src/components/Form/Input/index.js
+++ src/components/Form/Input/index.js
@@ -2,9 +2,9 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useField } from '@unform/core';
 
-// import { Container } from './styles';
+import { Label } from '../Label';
 
-export default function Input({ name, ...rest }) {
+export default function Input({ name, label, ...rest }) {
   const inputRef = useRef(null);
 
   const { fieldName, registerField } = useField(name);
@@ -17,9 +17,19 @@ export default function Input({ name, ...rest }) {
     });
   }, [fieldName, registerField]);
 
-  return <input ref={inputRef} {...rest} />;
+  return (
+    <>
+      {label && <Label htmlFor={fieldName}>{label}</Label>}
+      <input ref={inputRef} {...rest} />
+    </>
+  );
 }
 
 Input.propTypes = {
   name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+};
+
+Input.defaultProps = {
+  label: '',
 };
