f5aae71c4cf8877cc834b11c242a27445b266a6d
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-14 02:42:00 -0300

    ✨  feat: create the routes to views of recipients

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
-export default function Recipients() {
-  return <div />;
+export default function Recipients({ match }) {
+  return <Routes path={match.path} />;
 }
+
+Recipients.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+  }).isRequired,
+};
