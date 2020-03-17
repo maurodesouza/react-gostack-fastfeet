5741f8c8f2a2ccd12663c6127910d914ceed57ca
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-17 14:13:12 -0300

    ✨  feat: create atLeastTwoNames regex

--- src/util/regex.js
+++ src/util/regex.js
@@ -8,3 +8,5 @@ export const onlyTwoNames = fullName =>
   fullName.replace(/([a-zà-ú]+\s([a-zà-ú]{2,3}\s)?[a-zà-ú]+)(.*)/i, '$1');
 
 export const yet100Digits = text => text.replace(/(.{120})(.+)/, '$1...');
+
+export const atLeastTwoNames = /([a-zà-ú]+\s)([a-zà-ú]{2,3}\s)?([a-zà-ú]{4,})/i;
