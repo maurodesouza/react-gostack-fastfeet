b7fe76f5c26dc40dd17ba3e867250241e5d1ec85
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-18 00:06:15 -0300

    ✨  feat: create InputMask component

---
+++ src/components/Form/Inputs/InputMask.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+import ReactInputMask from 'react-input-mask';
+import PropTypes from 'prop-types';
+import { useField } from '@unform/core';
+
+import { Label } from '~/components/Form/Label';
+import { Error } from '~/components/Form/Error';
+
+export default function InputMask({ name, label, ...rest }) {
+  const inputRef = useRef(null);
+
+  const { fieldName, registerField, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+      setValue(ref, value) {
+        ref.setInputValue(value);
+      },
+      clearValue(ref) {
+        ref.setInputValue('');
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <span>
+      {label && <Label htmlFor={fieldName}>{label}</Label>}
+      <ReactInputMask ref={inputRef} {...rest} />
+      {error && <Error>{error}</Error>}
+    </span>
+  );
+}
+
+InputMask.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+};
+
+InputMask.defaultProps = {
+  label: '',
+};
--- src/components/Form/Inputs/index.js
+++ src/components/Form/Inputs/index.js
@@ -1,2 +1,3 @@
 export { default as Input } from './Input';
 export { default as AvatarInput } from './AvatarInput';
+export { default as InputMask } from './InputMask';
