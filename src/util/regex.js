dd0f9da63f283f5c96fd5587168f227593554f76
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-18 00:11:31 -0300

    ✨  feat: create validCep regex

--- src/util/regex.js
+++ src/util/regex.js
@@ -10,3 +10,5 @@ export const onlyTwoNames = fullName =>
 export const yet100Digits = text => text.replace(/(.{120})(.+)/, '$1...');
 
 export const atLeastTwoNames = /([a-zà-ú]+\s)([a-zà-ú]{2,3}\s)?([a-zà-ú]{4,})/i;
+
+export const validCep = /\d{5}-\d{3}/;
