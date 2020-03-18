3bb4329b5671d34c2660d6f6537cdf5aced41f8c
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-18 19:28:13 -0300

    🐛  fix: prop types error

--- src/components/Animation/index.js
+++ src/components/Animation/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { FadeIn } from './styles';
 
 export default function Animation({ children }) {
   return <FadeIn>{children}</FadeIn>;
 }
+
+Animation.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.instanceOf(Array),
+    PropTypes.element,
+  ]).isRequired,
+};
